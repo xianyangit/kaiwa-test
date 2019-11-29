b4d112cb73aa1e1ec845093e5510e4fa9ec9f9da
Xian Yang <99757057@qq.com>
2019-11-29 09:00:00 +0800

    chafen

--- common/js/search_models.js
+++ common/js/search_models.js
@@ -4,10 +4,20 @@ var csvValue;
 var qID = 1;
 var csvData = '/ra/app2/common/js/adapter.csv';
 
+var csvValuewl;
+var csvDatawl = '/ra/app/common/js/adapter.csv';
+
+
+
 $(function(){
 	//csv読み込み
 	$.get(csvData,function(data){
 		csvValue = $.csv(",", "", "\r\n")(data);
+	})
+	
+	//csv読み込み
+	$.get(csvDatawl,function(data){
+		csvValuewl = $.csv(",", "", "\r\n")(data);
 	})
 	
 });
@@ -24,6 +34,13 @@ function showData() {
 		var modelNum = this[0].slice(4);
 		if ( modelNum.match( reg )){
 			rslt += "<span style='font-size:18px;'>白くまくんアプリに対応しています。";
+			return false;
+		}
+	})
+	$(csvValuewl).each(function(){
+		var modelNum = this[0].slice(4);
+		if ( modelNum.match( reg )){
+			rslt += '<span style="font-size:18px;">日立エアコン モバイルコントロールに対応しています。詳しくは<a class="app_links_txt" href="/ra/app/index.html">こちら</a>しくはこちらをご確認ください。</span>';
 			return false;
 		}
 	})
